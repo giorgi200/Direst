d26a1fbacb324ce3f70ef8800112cbea668e221d
giorgi200 <giorgi.kochiashvili06@geolab.edu.ge>
2018-06-20 13:54:14 +0400

    markdown

---
+++ pm.md
@@ -0,0 +1,30 @@
+- `1. Homepage`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ✔
+- `2. Dishes`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ✔
+- `3. Restaurants`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ✔
+- `4. Dish`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ✔
+- `5. Restaurant`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ×
+- `6. Finished`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ×
+- `7. Dish success`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ×
+- `9. Choose dish type`
+-    ↳ Mobile    ×
+-    ↳ Desktop   ×
+
+- `❗ სად უნდა ეკლიკებოდეს პროდუქტს? `
+- `❗ jQuery ით სელექთების დასტილვა`
+- `❗ Web `
+- `❗ სად უნდა ეკლიკებოდეს პროდუქტს? `
+
--- singleRestaurant.php
+++ singleRestaurant.php
@@ -6,7 +6,7 @@
     <main class="container single-dish">
         <section class="dish-com d-flex justify-content-between">
             <div class="img-sec">
-                <img src="assets/img/download.jpeg" alt="">
+                <img src="assets/img/sorella-clapham-5.jpg" alt="">
             </div>
             <div class="details-sec">
                 <div class="d-flex  justify-content-between pre-head">
@@ -17,7 +17,28 @@
                 <p class="single-res">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
-                
+                <div class="call-dir d-flex justify-content-between">
+                        <!-- call -->
+                        <div class="call-loc d-flex">
+                            <img src="assets/img/to.svg" alt="">
+                            <p class="con-des">Call</p>   
+                        </div>
+                        <!-- Directions -->
+                        <div class="call-loc d-flex">
+                            <img src="assets/img/to.svg" alt="">
+                            <p class="con-des">Directions</p>   
+                        </div>
+                </div>
+                <!-- adress -->
+                <div class="d-flex loc-adress">
+                    <img src="assets/img/to.svg" alt="">
+                    <p class="adress">Vakhtang gorgasali str. 17</p>
+                </div>
+                <!-- DISHES -->
+                <div class="dish-list">
+                    <h1 class="dish-list-header">Dishes</h1>
+                    <?php require_once('layout/dishesdd.php'); ?>  
+                </div>
             </div>
         </section>
     </main>
